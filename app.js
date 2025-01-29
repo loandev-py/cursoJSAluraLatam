49147378743761a99906e44dd234f994c16f50a7
Hilker Bazan Flores <loandevpy@gmail.com>
2025-01-28 23:28:05 -0500

    agregamos un contador de intentos

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //Variables
 let numeroSecreto = 5;
 var contadorTentativas = 0;
+let intentos = 1;
+let palabraVeces = 'vez';
+
 while (numeroUsuario != numeroSecreto) {
 
     let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
@@ -12,13 +15,16 @@ while (numeroUsuario != numeroSecreto) {
     */
     if (numeroUsuario == numeroSecreto) {
         //Acertamos, fue verdadera la condición
-        alert(`Acertaste, el número es: ${numeroUsuario}`);
+        alert(`Acertaste, el número es: ${numeroUsuario}. lo hiciste en ${intentos} ${palabraVeces}`);
     } else {
         if (numeroUsuario > numeroSecreto){
             alert(`El número secreto es menor`);
         } else {
             alert(`El número secreto es mayor`);
         }
+        //Incrementamos el contador cunado no acierta
+        intentos = intentos + 1
+        palabraVeces = 'veces';
         //La condición no se cumplió
         //alert('Lo siento, no acertaste el número');
     }
