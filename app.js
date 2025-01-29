4322afaa0ecfb148b18833bee705fe6e115a1f66
Hilker Bazan Flores <loandevpy@gmail.com>
2025-01-28 23:10:19 -0500

    podemos interactuar varias veces hasta que acerte

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 //Variables
 let numeroSecreto = 5;
-let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+var contadorTentativas = 0;
+while (numeroUsuario != numeroSecreto) {
 
-console.log(numeroUsuario);
-/*
-Este código realiza
-la comparación
-*/
-if (numeroUsuario == numeroSecreto) {
-    //Acertamos, fue verdadera la condición
-    alert(`Acertaste, el número es: ${numeroUsuario}`);
-} else {
-    if (numeroUsuario > numeroSecreto){
-        alert(`El número secreto es menor`);
+    let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+
+    console.log(numeroUsuario);
+    /*
+    Este código realiza
+    la comparación
+    */
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}`);
     } else {
-        alert(`El número secreto es mayor`);
+        if (numeroUsuario > numeroSecreto){
+            alert(`El número secreto es menor`);
+        } else {
+            alert(`El número secreto es mayor`);
+        }
+        //La condición no se cumplió
+        //alert('Lo siento, no acertaste el número');
     }
-    //La condición no se cumplió
-    //alert('Lo siento, no acertaste el número');
 }
